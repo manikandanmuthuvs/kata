e4d100a6909d21ba4dce2285fc6a7ea1a3131051
Manikandan Muthuvs <manikandan.muthuvs@gmail.com>
2019-07-28 17:41:35 +0530

    added code to pass the failing unit test

--- java-spring-xp/src/main/java/com/manikandanmuthuvs/javaspringxp/service/UserServiceImpl.java
+++ java-spring-xp/src/main/java/com/manikandanmuthuvs/javaspringxp/service/UserServiceImpl.java
@@ -3,15 +3,15 @@ package com.manikandanmuthuvs.javaspringxp.service;
 public class UserServiceImpl implements UserService {
 
     public String UserName(Integer usernameCode) {
-        if (usernameCode % 3 == 0) {
+        if (usernameCode == 0)
+            return "0";
+        if (usernameCode % 3 == 0)
             return "Fizz";
-        } else if (usernameCode % 5 == 0) {
+        if (usernameCode % 5 == 0)
             return "Buzz";
-        } else if (usernameCode % 3 == 0 && usernameCode % 5 == 0) {
+        if (usernameCode % 3 == 0 && usernameCode % 5 == 0)
             return "Fizz Buzz";
-        } else {
-            return "Error";
-        }
+        return "Error";
     }
 
 }
