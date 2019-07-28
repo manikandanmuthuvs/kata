7cab1eb58c10abbc6a47dbec2c9750f3ef8037fb
Manikandan Muthuvs <manikandan.muthuvs@gmail.com>
2019-07-28 16:07:42 +0530

    added a failing behavior test first

--- java-spring-xp/src/test/java/com/manikandanmuthuvs/javaspringxp/integrationtest/UserServiceIT.java
+++ java-spring-xp/src/test/java/com/manikandanmuthuvs/javaspringxp/integrationtest/UserServiceIT.java
@@ -9,9 +9,14 @@ import static org.junit.Assert.assertEquals;
 
 public class UserServiceIT extends SpringIntegrationTest {
     
+    private String rootUri = "http://localhost:8081";
+
     @Given("^the user has full name \"([^\"]*)\"$")
     public void the_user_has_full_name(String arg1) throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    }
+    
+    @When("^the Client request User servie /user/firstname\\$ with code (\\d+)$")
+    public void the_Client_request_User_servie_user_firstname$_with_code(int usernameCode) throws Exception {
+        executeGet(rootUri+"/hello/?usercode="+usernameCode);
     }
 }
