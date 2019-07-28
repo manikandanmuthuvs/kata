f434ad936841a0bcb129e77b08999b0d184ac6ba
Manikandan Muthuvs <manikandan.muthuvs@gmail.com>
2019-07-28 16:31:52 +0530

    added a failing behavior test first

--- java-spring-xp/src/test/java/com/manikandanmuthuvs/javaspringxp/integrationtest/UserServiceIT.java
+++ java-spring-xp/src/test/java/com/manikandanmuthuvs/javaspringxp/integrationtest/UserServiceIT.java
@@ -10,13 +10,19 @@ import static org.junit.Assert.assertEquals;
 public class UserServiceIT extends SpringIntegrationTest {
     
     private String rootUri = "http://localhost:8081";
-
-    @Given("^the user has full name \"([^\"]*)\"$")
-    public void the_user_has_full_name(String arg1) throws Exception {
+ 
+    @Given("^the User has full name \"([^\"]*)\"$")
+    public void the_User_has_full_name(String arg1) throws Exception {        
     }
-    
+
     @When("^the Client request User servie /user/firstname\\$ with code (\\d+)$")
     public void the_Client_request_User_servie_user_firstname$_with_code(int usernameCode) throws Exception {
-        executeGet(rootUri+"/hello/?usercode="+usernameCode);
+        executeGet(rootUri + "/hello/?usercode=" + usernameCode);
+    }
+
+    @Then("^the Client receives User first name \"([^\"]*)\"$")
+    public void the_Client_receives_User_first_name(String arg1) throws Exception {
+        final HttpStatus currentStatusCode = latestResponse.getTheResponse().getStatusCode();
+        assertEquals(true, true);
     }
 }
