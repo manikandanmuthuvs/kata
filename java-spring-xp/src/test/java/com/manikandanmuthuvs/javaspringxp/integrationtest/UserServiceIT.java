7a84049c21b83e856713cf807b52c175b0660f98
Manikandan Muthuvs <manikandan.muthuvs@gmail.com>
2019-07-28 16:00:09 +0530

    added a failing behavior test first

--- java-spring-xp/src/test/java/com/manikandanmuthuvs/javaspringxp/integrationtest/UserServiceIT.java
+++ java-spring-xp/src/test/java/com/manikandanmuthuvs/javaspringxp/integrationtest/UserServiceIT.java
@@ -9,4 +9,9 @@ import static org.junit.Assert.assertEquals;
 
 public class UserServiceIT extends SpringIntegrationTest {
     
+    @Given("^the user has full name \"([^\"]*)\"$")
+    public void the_user_has_full_name(String arg1) throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
 }
